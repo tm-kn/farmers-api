fd77e3211e2298457b9778f409c56c70a36bf3db
Tomasz Knapik <tmkn0808@gmail.com>
2016-12-17 19:56:16 +0000

    Add permission settings on FarmerViewSet

--- farmers_api/farmers/views.py
+++ farmers_api/farmers/views.py
@@ -1,10 +1,11 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 
 from .models import Farmer
 from .serializers import FarmerSerializer
 
 
-class FarmerViewSet(viewsets.ReadOnlyModelViewSet):
+class FarmerViewSet(viewsets.ModelViewSet):
     queryset = Farmer.objects.all()
     serializer_class = FarmerSerializer
     filter_fields = ('town',)
+    permissions = permissions.DjangoModelPermissionsOrAnonReadOnly
