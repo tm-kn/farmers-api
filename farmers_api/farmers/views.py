0fd7182336ad516634635da41b61f623ca04f4bc
Tomasz Knapik <tmkn0808@gmail.com>
2016-12-17 19:06:05 +0000

    Add serializer and viewset for the farmers resource.

---
+++ farmers_api/farmers/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from .models import Farmer
+from .serializers import FarmerSerializer
+
+
+class FarmerViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Farmer.objects.all()
+    serializer_class = FarmerSerializer
