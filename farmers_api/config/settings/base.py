7547741403d17ac5c224b88f5ccdde974e194170
Tomasz Knapik <tmkn0808@gmail.com>
2016-12-17 19:17:14 +0000

    Add django-filter to requirements and settings

--- farmers_api/config/settings/base.py
+++ farmers_api/config/settings/base.py
@@ -77,3 +77,9 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    )
+}
